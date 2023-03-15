58258d7da72cf9cf9d94c22e2f07e6d14c94c703
impaseri <pseri1224@gmail.com>
2023-03-15 17:47:49 +0900

    main2

--- js/main.js
+++ js/main.js
@@ -34,14 +34,14 @@ $(function(){
 
 
 const docuE = document.documentElement;
-const newCursor = document.getElementById('cursor')
+const newCursor = document.getElementById('cursor');
 let posX = 0;
 let posY = 0;
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.onmousemove = function(e) {
-        posX = e.clientX + "px";
-        posY = e.clientY + "px";
+document.addEventListener('DOMContentLoaded', () =>{
+    document.documentElement.onmousemove = function(e){
+        posX = e.pageX + "px";
+        posY = e.pageY + "px";
 
         newCursor.style.left = posX;
         newCursor.style.top = posY;
